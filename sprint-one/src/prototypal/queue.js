ae09d4119d8f31fa77c0a1afa8b56012ee1ef120
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:27:57 -0700

    Prototypal Queue is meeting specification

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
+  
+  var instanceOf = Object.create(queueMethods);
+  instanceOf.storage = {};
+  return instanceOf;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var queueMethods = {};
+queueMethods.dequeue = function() {
+  var temp = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return temp;
+};
+
+queueMethods.enqueue = function(value) {
+  this.storage[value] = value;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
 
