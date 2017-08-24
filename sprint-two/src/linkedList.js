f483518322f907994a8447c342105ee6a770c817
Hack Reactor Student <student@hackreactor.com>
2017-08-24 10:41:25 -0700

    Completed test 4

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,16 +1,33 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = {};
+  list.head = {
+    value: null
+
+  };
+  list.tail = {
+    listedValues: []
+  };
 
   list.addToTail = function(value) {
-    list.tail.value = value;
+    list.tail.listedValues.push(value);
+    list.tail.value = list.tail.listedValues[list.tail.listedValues.length - 1];
+    list.head.value = list.tail.listedValues[0];
   };
 
   list.removeHead = function() {
+    /*
+    var temp = Node(list.head[Object.keys(list.head)[0]];
+    delete list.head[Object.keys(list.head)[0]];
+    return temp;
+    */
+    var temp = list.tail.listedValues.shift();
+    list.head.value = list.tail.listedValues[0];
+    return temp;
+    
   };
 
   list.contains = function(target) {
+  //search for target
   };
 
   return list;
