ded45c9acd746e5af798d89e3ddd67cf88d8b85a
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:05:32 -0700

    finished functional queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,32 @@
 var Queue = function() {
+  var instance = {};
+  
+  instance.storage = {};
+  
+  _.extend(instance, queueMethods);
+
+  return instance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[value] = value;
+};
+
+queueMethods.dequeue = function() {
+  var temp = this.storage[Object.keys(this.storage)[0]];
+  delete this.storage[Object.keys(this.storage)[0]];
+  return temp;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+
 
 
