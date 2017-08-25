e9b5ee5b9f684e2d7e41b85e9078ac512774f447
Hack Reactor Student <student@hackreactor.com>
2017-08-25 16:50:53 -0700

    Binary serach started

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,25 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(binaryMethods);
+  this.right = undefined;
+  this.left = undefined;
+  this.value = value;
+
+  return tree;
+};
+
+var binaryMethods = {};
+binaryMethods.insert = function(value) {
+  var newTree = BinarySearchTree(value);
+  console.log(newTree);
+  return newTree;
+};
+
+binaryMethods.contains = function(target) {
+
+};
+
+binaryMethods.depthFirstLog = function() {
+
 };
 
 
